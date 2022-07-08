d95501740054eacca75847158df834779e896697
jingdaxu <3130705067@qq.com>
2022-07-08 17:27:02 +0800

    首页更新

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
+  background: #f2f3f5;
+  height: 100vh;
 }
 </style>
--- src/view/home.vue
+++ src/view/home.vue
@@ -2,11 +2,12 @@
   <!-- 首页 -->
   <div>
     <van-row>
-      <van-col span="24"> HOME </van-col>
+      <van-col span="8"> HOME </van-col>
     </van-row>
     <!-- TAB -->
     <van-row>
-      <van-col span="8"> 月收益率 总收益率 </van-col>
+      <van-col span="8"> 月收益率 </van-col>
+      <van-col span="8"> 总收益率 </van-col>
     </van-row>
     <van-list
       v-model:loading="loading"
@@ -15,8 +16,8 @@
       @load="onLoad"
     >
       <!-- LIST -->
-      <div v-for="(item, index) in List" :key="index">
-        <van-row gutter="20">
+      <div v-for="(item, index) in List" :key="index" class="list-box">
+        <van-row gutter="20" class="row1">
           <van-col span="4">
             <VanImage
               round
@@ -30,7 +31,7 @@
             <div class="text-left">{{ item.signature }}</div>
           </van-col>
         </van-row>
-        <van-row gutter="20" justify="space-between">
+        <van-row gutter="20" justify="space-between" class="row2">
           <van-col span="8">
             <div class="black">￥{{ item.accountMoney }}</div>
             <div class="text">账户资金</div>
@@ -97,6 +98,20 @@ export default {
 </script>
 
 <style>
+.list-box {
+  background-color: white;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 15px;
+}
+.row1 {
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ebedf0;
+}
+.row2 {
+  margin: 10px 0;
+}
 .text-left {
   text-align: left;
 }
